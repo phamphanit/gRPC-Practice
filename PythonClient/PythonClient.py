37ed163b0ffc1126a47300b1632baee105cdf571
Pham Phan <phamphanthsp@gmail.com>
2021-03-13 18:05:27 +0700

    grpc client with python

--- PythonClient/PythonClient.py
+++ PythonClient/PythonClient.py
@@ -5,13 +5,19 @@ import MeterReader_pb2 as MeterReader
 import MeterReader_pb2_grpc as MeterReaderService
 def main():
 	print("Calling gRPC Service")
-	channel = grpc.insecure_channel("localhost:5001")
+	with open("localhost.cer","rb") as file:
+		cert = file.read()
+
+	credentials = grpc.ssl_channel_credentials(cert)
+	channel = grpc.secure_channel("localhost:5001",credentials)
 	stub = MeterReaderService.MeterReadingServiceStub(channel)
 
+	now = Timestamp()
+	now.GetCurrentTime()
 	request = MeterReader.ReadingPacket(successful = Enums.ReadingStatus.SUCCESS)
 	reading = MeterReader.ReadingMessage(customerId = 1,
-									  readingValue = 1,
-									  readingTime = Timestamp().GetCurrentTime())
+					readingValue = 1,
+					readingTime = now)
 
 	request.readings.append(reading)
 
